5b3cccd8c9d515b3cfa2f800d123c49fd4866d81
Armin Vieweg <armin@v.ieweg.de>
2026-05-06 13:23:10 +0200

    [TASK] Add SizeToolbarItem

---
+++ Classes/Backend/Toolbar/SizeToolbarItem.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace T3\Size\Backend\Toolbar;
+
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Toolbar\RequestAwareToolbarItemInterface;
+use TYPO3\CMS\Backend\Toolbar\ToolbarItemInterface;
+use TYPO3\CMS\Backend\View\BackendViewFactory;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+
+final class SizeToolbarItem implements ToolbarItemInterface, RequestAwareToolbarItemInterface
+{
+    private ServerRequestInterface $request;
+
+    public function __construct(
+        private readonly IconFactory $iconFactory,
+        private readonly BackendViewFactory $backendViewFactory,
+    ) {}
+
+    public function setRequest(ServerRequestInterface $request): void
+    {
+        $this->request = $request;
+    }
+
+    public function checkAccess(): bool
+    {
+        // Hier steuerst du, wer den Button sehen darf.
+        // Beispiel: alle eingeloggten Backend-User.
+        return isset($GLOBALS['BE_USER']) && $GLOBALS['BE_USER']->user;
+    }
+
+    public function getItem(): string
+    {
+        $view = $this->backendViewFactory->create($this->request, ['t3/size']);
+
+        return $view->render('ToolbarItems/SizeToolbarItem');
+
+
+//        $icon = $this->iconFactory->getIcon(
+//            'actions-info',
+//            IconSize::SMALL
+//        )->render();
+//
+//        return '
+//            <button
+//                type="button"
+//                class="toolbar-item-link dropdown-toggle"
+//                title="Projektinfo"
+//                aria-label="Projektinfo"
+//            >
+//                ' . $icon . '
+//            </button>
+//        ';
+    }
+
+    public function hasDropDown(): bool
+    {
+        return true;
+    }
+
+    public function getDropDown(): string
+    {
+        $view = $this->backendViewFactory->create($this->request, ['t3/size']);
+
+        return $view->render('ToolbarItems/SizeToolbarItemDropDown');
+
+//        return '
+//            <div class="dropdown-menu dropdown-menu-end">
+//                <div class="dropdown-header">Projektinfo</div>
+//                <div class="dropdown-item-text">
+//                    <strong>Deployment:</strong><br>
+//                    v2026.05.06
+//                </div>
+//                <div class="dropdown-divider"></div>
+//                <a class="dropdown-item" href="/typo3/module/web/layout">
+//                    Zum Seitenmodul
+//                </a>
+//            </div>
+//        ';
+    }
+
+    public function getAdditionalAttributes(): array
+    {
+        return [
+//            'class' => 'toolbar-item-size',
+        ];
+    }
+
+    public function getIndex(): int
+    {
+        return 50;
+    }
+}
