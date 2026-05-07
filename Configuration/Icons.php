e9d2ae23e4e6cded6a915540f934428cf9a8c63c
Armin Vieweg <armin@v.ieweg.de>
2026-05-07 14:38:47 +0200

    [TASK] Update toolbar icon

---
+++ Configuration/Icons.php
@@ -0,0 +1,11 @@
+<?php
+
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'tx-size-chart-pie' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:size/Resources/Public/Icons/chart-pie-solid-full.svg',
+    ],
+];
